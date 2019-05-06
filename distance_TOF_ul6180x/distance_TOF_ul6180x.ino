f069f85750659affd85ecd98d82b6f59866851b7
Tim Hebert <hedlesschkn@gmail.com>
2019-05-06 15:57:10 -0400

    added file

---
+++ distance_TOF_ul6180x/distance_TOF_ul6180x.ino
@@ -0,0 +1,17 @@
+/*
+ * UL6180x TOF mini-lidar range finder distance sensor
+ * code by Tim Hebert
+ * 5/6/2019
+ * using library: pololu/vl6180x-arduino
+ * 
+ * wiring
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
